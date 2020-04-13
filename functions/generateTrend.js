3fc8e21f7d1b04f6c0c69d2ac62b69f5241f6312
limyifan1 <limyifan1@gmail.com>
2020-04-14 06:07:15 +0800

    Added two graphs for trend

--- functions/generateTrend.js
+++ functions/generateTrend.js
@@ -1,52 +1,97 @@
-// 600x600 canvas size
-var stackedBarChartSpec = require('./stacked-bar-chart.spec.json');
-var data = require('./data.spec.json');
-
+const fetch = require('cross-fetch');
+var cumulative = require('./cumulative.spec.json');
+var daily = require('./daily.spec.json');
 var vega = require('vega')
 var fs = require('fs')
 
-let date
-let compiled = {}
-let count = 0
-data.features.forEach(element => {
+// var data = require('./data.spec.json');
+
+async function getData(params) {
+  return fetch('https://services6.arcgis.com/LZwBmoXba0zrRap7/arcgis/rest/services/COVID_19_Prod_B_cumulative/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&resultOffset=0&resultRecordCount=2000&cacheHint=true')
+  .then((response) => {
+      return response.json()
+  })
+  .catch(error => {
+      console.log(error)
+  })
+}
+
+async function retrieveData() {
+  let data =  await getData()
+  let date
+  let compiled = {}
+  let count = 0
+  data.features.forEach(element => {
     date = new Date(element.attributes.Date).toLocaleDateString('EN-SG', {timeZone: "Asia/Shanghai"})
-    stackedBarChartSpec.data[0].values.push({
-        // "x": date, 
-        "x": count,
-        "y": element.attributes.Confirmation_Total - element.attributes.Discharge_Total, 
-        "c": 1
-    })
-    stackedBarChartSpec.data[0].values.push({
+    if (element.attributes.Discharge_Total >= 100){
+      cumulative.data[0].values.push({
         // "x": date, 
         "x": count,
         "y": element.attributes.Discharge_Total, 
-        "c": 0
-    })
-    count+=1
-    element.attributes.Date = date
-    compiled[date] = element.attributes
-});
-
-var view = new vega
-  .View(vega.parse(stackedBarChartSpec))
-  .renderer('none')
-  .initialize();
-
-// generate static PNG file from chart
-view
-  .toCanvas()
-  .then((canvas) => {
-    // process node-canvas instance for example, generate a PNG stream to write var
-    stream = canvas.createPNGStream();
-    console.log('Writing PNG to file...')
-    fs.writeFile('stackedBarChart.png', canvas.toBuffer(), ()=>{
-        console.log("Rendered File")
-    })
-    return true
-  })
-  .catch((err) => {
-    console.log("Error writing PNG to file:")
-    console.error(err)
+        "c": "Discharged Cases"
+      })
+      cumulative.data[0].values.push({
+        // "x": date, 
+        "x": count,
+        "y": element.attributes.Confirmation_Total - element.attributes.Discharge_Total, 
+        "c": "Active Cases"
+      })
+          daily.data[0].values.push({
+            // "x": date, 
+            "x": count,
+            "y": element.attributes.Confirmation_Volume, 
+            "c": 1
+          })
+          count+=1
+          element.attributes.Date = date
+          compiled[date] = element.attributes
+    }
   });
 
-// console.log(stackedBarChartSpec.data[0].values)
+  var view = new vega
+    .View(vega.parse(cumulative))
+    .renderer('none')
+    .initialize();
+
+  // generate static PNG file from chart
+  view
+    .toCanvas()
+    .then((canvas) => {
+      // process node-canvas instance for example, generate a PNG stream to write var
+      stream = canvas.createPNGStream();
+      console.log('Writing PNG to file...')
+      fs.writeFile('cumulative.png', canvas.toBuffer(), ()=>{
+          console.log("Rendered File")
+      })
+      return true
+    })
+    .catch((err) => {
+      console.log("Error writing PNG to file:")
+      console.error(err)
+    });
+
+    view = new vega
+    .View(vega.parse(daily))
+    .renderer('none')
+    .initialize();
+
+  // generate static PNG file from chart
+  view
+    .toCanvas()
+    .then((canvas) => {
+      // process node-canvas instance for example, generate a PNG stream to write var
+      stream = canvas.createPNGStream();
+      console.log('Writing PNG to file...')
+      fs.writeFile('daily.png', canvas.toBuffer(), ()=>{
+          console.log("Rendered File")
+      })
+      return true
+    })
+    .catch((err) => {
+      console.log("Error writing PNG to file:")
+      console.error(err)
+    });
+
+}
+
+retrieveData()
--- functions/trend.js
+++ functions/trend.js
@@ -4,14 +4,15 @@ async function getTrend(req, res){
     var today = new Date();
     var date = today.getDate() +'-'+(today.getMonth()+1) + '-' + today.getFullYear();
     const chat_id = req.body.message.chat.id
-    status = 'Here\'s the chart of the number of cases from January 23 to ' + 'today ' + date
+    status = 'Here\'s the chart of the number of cases from February to ' + 'today ' + date
     return res.status(200).send({
-        method: 'sendPhoto',
-        photo: 'https://firebasestorage.googleapis.com/v0/b/covidsgbot.appspot.com/o/stackedBarChart.png?alt=media&token=1e27d11e-1d0f-4643-ac17-b47b6d920307',
-        parse_mode: 'HTML',
+        method: 'sendMediaGroup',
+        media: [
+            'https://firebasestorage.googleapis.com/v0/b/covidsgbot.appspot.com/o/daily.png?alt=media&token=500e745b-a88d-4a29-babf-483b2c6ace02',
+            'https://firebasestorage.googleapis.com/v0/b/covidsgbot.appspot.com/o/cumulative.png?alt=media&token=f756ac3d-193f-4e57-b1ba-f8137565260e'
+        ],
         chat_id,
-        caption: status
-        })
+    })
 }
 
 
